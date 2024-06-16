3c1d5985804d80233635b1b6ec05616f27c58228
jude-n <77964136+jude-n@users.noreply.github.com>
2024-06-16 12:39:35 -0500

    Abstracted tabbable logic to a new component

---
+++ my-first-vue-project/src/components/TabbableTextArea.vue
@@ -0,0 +1,22 @@
+<script setup>
+function onTabPress(e) {
+
+  let t = e.target;
+  //   tab was pressed
+  //   get caret postion/selection
+  //   caret indicates where the user can enter text
+  let val = t.value,
+    start = t.selectionStart,
+    end = t.selectionEnd;
+
+  //   set textarea value to: text before caret + tab + text after caret
+  t.value = val.substring(0, start) + "\t" + val.substring(end);
+
+  //   put caret at right position again
+  t.selectionStart = t.selectionEnd = start + 1;
+}
+</script>
+
+<template>
+  <textarea @keydown.tab.prevent="onTabPress"></textarea>
+</template>
--- my-first-vue-project/src/views/HomeView.vue
+++ my-first-vue-project/src/views/HomeView.vue
@@ -1,32 +1,12 @@
 <script setup>
-
-import { onMounted, ref } from 'vue'; // like the key attribute
-
-// Linked to the whole textarea
-let textarea = ref(null);
-
-function onTabPress(e) {
-
-    let t = e.target;
-      //   tab was pressed
-      //   get caret postion/selection
-      //   caret indicates where the user can enter text
-      let val = t.value,
-        start = t.selectionStart,
-        end = t.selectionEnd;
-
-      //   set textarea value to: text before caret + tab + text after caret
-      t.value = val.substring(0, start) + "\t" + val.substring(end);
-
-      //   put caret at right position again
-      t.selectionStart = t.selectionEnd = start + 1;
-}
+import TabbableTextArea from '@/components/TabbableTextArea.vue';
 </script>
 
 <template>
   <main>
     <form>
-      <textarea @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px;">Hi there</textarea>
+<!--      passing attributes that arent props to a custom component sends it straight to the root element. in this case the text area-->
+      <TabbableTextArea style="width: 100%; height: 300px" />
     </form>
   </main>
 </template>

d9924869befc5abafc4becad42b0694752bac99f
jude-n <77964136+jude-n@users.noreply.github.com>
2024-06-25 13:26:47 -0500

    Can easily be added to other components

--- my-first-vue-project/src/components/Quiz/QuizHeader.vue
+++ my-first-vue-project/src/components/Quiz/QuizHeader.vue
@@ -1,11 +1,14 @@
 <template>
-  <main>
-    <h3>Quiz Header</h3>
-    <p>Welcome to your first quiz. How do you think you'll do ?</p>
-  </main>
+  <header>
+    <h1>{{  state.name }}</h1>
+
+    <p>
+      Welcome to your first quiz. How do you think you'll do ?
+    </p>
+  </header>
 
 </template>
 
 <script setup>
-
+import { state } from "@/stores/quizStore";
 </script>
